81924410b9f20bc57a21e04d1f0d36c6133d0568
Md farhaf alom <88694159+farhadalom@users.noreply.github.com>
2021-08-15 05:24:50 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,68 @@
+ $(document).ready(function(){
+     $(window).scroll(function(){
+       if(this.scrollY > 20){
+         $('.navbar').addClass('sticky');
+       }
+       else{
+        $('.navbar').removeClass('sticky');
+       }
+       if(this.scrollY > 500){
+        $('.scroll-up-btn').addClass('show'); 
+       }
+       else{
+        $('.scroll-up-btn').removeClass('show'); 
+       }
+     });
+
+     //slide up script
+     $('.scroll-up-btn').click(function(){
+       $('html').animate({scrollTop: 0});
+     }); 
+
+
+     /*navbar toggleclass*/
+     $('.menu-btn').click(function(){
+         $('.navbar .menu').toggleClass('show');
+         $('.menu-btn i').toggleClass('show'); 
+     });
+    
+
+      //type animation
+
+      var typed = new Typed(".typing",{
+        strings: ["Youtuber","Developer", "Designer","blogger", "Freelancher"],
+        typeSpeed: 100,
+        backSpeed:60,
+        loop: true
+      })
+      var typed = new Typed(".typing-2",{
+        strings: ["Youtuber","Developer", "Designer","blogger", "Freelancher"],
+        typeSpeed: 100,
+        backSpeed:60,
+        loop: true
+      })
+
+     //owl carousel script
+     $('.carousel').owlCarousel({
+      margin: 10,
+      loop: true,
+      autoplayTimeOut: 2000,
+      autoplayHoverPause: true,
+      responsive: {
+        0:{
+          items: 1,
+          nav: false
+        },
+        600:{
+          items: 2,
+          nav: false
+        },
+        1000:{
+          items: 3,
+          nav: false
+        } 
+      }
+
+     });
+ });
+ 
